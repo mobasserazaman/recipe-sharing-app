4ab83bd92d8c683f23178c47640e800d44034964
Mobassera Zaman <mobassera.zaman@mail.mcgill.ca>
2024-08-25 13:57:27 -0400

    Trying to fix embedded git repo

---
+++ client/src/pages/home.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { useGetUserID } from "../hooks/useGetUserID";
+import axios from "axios";
+import Heart from "react-heart"
+
+export const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState([]);
+
+  const userID = useGetUserID();
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/recipes");
+        setRecipes(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    const fetchSavedRecipes = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3001/recipes/savedRecipes/ids/${userID}`
+        );
+        setSavedRecipes(response.data.savedRecipes);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchRecipes();
+    fetchSavedRecipes();
+  }, []);
+
+  const saveRecipe = async (recipeID) => {
+    try {
+      const response = await axios.put("http://localhost:3001/recipes", {
+        recipeID,
+        userID,
+      });
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteRecipe = async (recipeID) => {
+    try {
+      const response = await axios.put("http://localhost:3001/recipes/remove", {
+        recipeID,
+        userID,
+      });
+      setSavedRecipes(response.data.savedRecipes);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const isRecipeSaved = (id) => savedRecipes.includes(id);
+
+  return (
+    <div className="homepage">
+      <h2>Recipes</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe._id}>
+            <div>
+              <h2>{recipe.name}</h2>
+              <Heart style={{height:'20px', width:'20px'}} isActive={isRecipeSaved(recipe._id)} onClick={() => {isRecipeSaved(recipe._id)? deleteRecipe(recipe._id):saveRecipe(recipe._id)}}/>
+              {/* <button
+                onClick={() => saveRecipe(recipe._id)}
+                disabled={isRecipeSaved(recipe._id)}
+              >
+                {isRecipeSaved(recipe._id) ? "Saved" : "Save"}
+              </button> */}
+            </div>
+            <div className="instructions">
+              <p>{recipe.instructions}</p>
+            </div>
+            <img src={recipe.imageUrl} alt={recipe.name} />
+            <p>Cooking Time: {recipe.cookingTime} minutes</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
